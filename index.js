dac025f425386e89acf21590be89894d649e4d9b
cristianfortuaco-design <cristian.fortu.aco@gmail.com>
2026-07-16 16:41:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,11 @@ app.get('/check-ip', async (req, res) => {
     const data = await response.json();
     res.send("LA IP REAL DE TU SERVIDOR ES: " + data.ip);
 });
+app.get('/mi-ip', async (req, res) => {
+    const response = await fetch('https://api.ipify.org');
+    const ip = await response.text();
+    res.send("LA IP REAL ES: " + ip);
+});
+
 app.listen(PORT, () => console.log(`Proxy corriendo correctamente en el puerto ${PORT}`));
 app.listen(PORT, () => console.log(`Proxy corriendo en el puerto ${PORT}`));
